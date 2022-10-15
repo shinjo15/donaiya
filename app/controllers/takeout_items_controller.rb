bdcf9b30244ce53aa9beb12ecec9b26256437f17
shinjo15 <106855531+shinjo15@users.noreply.github.com>
2022-10-15 13:46:17 +0000

    Update Rwcommendations New

--- app/controllers/takeout_items_controller.rb
+++ app/controllers/takeout_items_controller.rb
@@ -1,5 +1,13 @@
 class TakeoutItemsController < ApplicationController
   def new
+    @takeout_item = Takeout.new
+  end
+
+  def create
+    takeout_item = Takeout.new(takeout_items_params)
+    takeout_item.save
+    redirect_to edit_takeout_item(takeout_item.id)
+
   end
 
   def index
@@ -9,5 +17,11 @@ class TakeoutItemsController < ApplicationController
   end
 
   def show
+  end
+
+  private
+
+  def takeout_items_params
+    params.require(:takeout_item).permit(:image, :takeout_name, :tax_price, :takeout_details, :is_active)
   end
 end
