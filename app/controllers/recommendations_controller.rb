eae875ca58013ff6f89c4779da5e8d48b6a57969
shinjo15 <106855531+shinjo15@users.noreply.github.com>
2022-10-15 08:05:50 +0000

    Add Recommendation New

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -1,10 +1,30 @@
 class RecommendationsController < ApplicationController
   def new
+    @food = Recommendation.new
+    @foods = Recommendation.all
+  end
+
+  def create
+    food = Recommendation.new(recommendations_params)
+    food.save
+    redirect_to new_recommendation_path
   end
 
   def index
   end
 
   def edit
+  end
+
+  def destroy
+    @food = Recommendation.find(params[:id])
+    @food.destroy
+    redirect_to new_recommendation_path
+  end
+
+  private
+
+  def recommendations_params
+    params.require(:recommendation).permit(:image, :name, :tax_price, :details)
   end
 end
