c6d8ea799c00c1978840cb2c61de98081ac2d5e2
shinjo15 <106855531+shinjo15@users.noreply.github.com>
2022-11-02 12:21:22 +0000

    Update CartItem Index

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -4,16 +4,20 @@ class CartItemsController < ApplicationController
   end
 
   def create
-   cart = current_cart
-   takeout_item = TakeoutItem.find(params[:takeout_item_id])
-   @buy_item = cart.add_takeout_item(takeout_item.id)
-   @buy_item.save
-   redirect_to cart_items_path
+    cart = current_cart
+    takeout_item = TakeoutItem.find(params[:takeout_item_id])
+    @buy_item = cart.add_takeout_item(takeout_item.id)
+    unless @buy_item.amount
+      @buy_item.amount = 0
+    end
+    @buy_item.amount += cart_item_params[:amount].to_i
+    @buy_item.save
+    redirect_to takeout_item_cart_items_path
   end
 
   private
 
   def cart_item_params
-    params.require(:cart_item).permit(:takeout_item_id, :amount)
+    params.require(:cart_item).permit(:amount)
   end
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,8 +1,8 @@
 class Cart < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :takeout_item, through: :cart_items
-  
-  def add_takeout_item(takeout_item_id)
-    cart_items.find_or_initialize_by(tekeout_item_id: tekeout_item_id)
+
+  def add_takeout_item(item_id)
+    cart_items.find_or_initialize_by(takeout_item_id: item_id)
   end
 end
